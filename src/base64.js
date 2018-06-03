f518e17e22e1ef1864b8fec6c942be53134b3bed
Anthony Bocci <boccianthony@yahoo.fr>
2018-06-03 15:47:56 +0200

    Encode/Decode base64

--- src/base64.js
+++ src/base64.js
@@ -1,14 +1,31 @@
 export class Base64 {
+    /**
+     * Construct the base64 object.
+     *
+     * 'string' is the string written in a textearea, the one we want to
+     * encode/decode.
+     * 'result' is the result of the encoding/decoding.
+     */
     constructor() {
         this.string = '';
         this.result = '';
     }
 
+    /**
+     * Encode 'string' to base64.
+     *
+     * Work with unicode.
+     */
     encode() {
-        this.result = 'encoded value';
+        this.result = btoa(unescape(encodeURIComponent(this.string)));
     }
 
+    /**
+     * Decode 'string' from base64.
+     *
+     * Work with unicode.
+     */
     decode() {
-        this.result = 'decoded value';
+        this.result = decodeURIComponent(escape(atob(this.string)));
     }
 }
