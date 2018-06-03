639e1e2c9ab51040e35524dfeba65d10ec94c10c
Anthony Bocci <boccianthony@yahoo.fr>
2018-06-03 13:01:37 +0200

    Prepare the base64 encoding/decoding
    Everything works, only the base64 encoding/decoding in the JavaScript is
    missing.

---
+++ src/base64.js
@@ -0,0 +1,14 @@
+export class Base64 {
+    constructor() {
+        this.string = '';
+        this.result = '';
+    }
+
+    encode() {
+        this.result = 'encoded value';
+    }
+
+    decode() {
+        this.result = 'decoded value';
+    }
+}
